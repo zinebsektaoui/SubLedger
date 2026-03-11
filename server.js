12819a13d86e3a929dd2f17069c8c23c0cb2d428
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-03-11 13:38:00 +0000

    Lancement du server + connexion à mongodb

--- server.js
+++ server.js
@@ -1,11 +1,23 @@
 const express = require("express")
+require("dotenv").config()
+const mongoose = require("mongoose")
+
+const app = express()
+
 const userRoutes = require("./routes/user.route")
 const adminRoutes = require("./routes/admin.route")
 const subscriptionRoutes = require("./routes/subscription.route")
-const app = express()
 
 app.use(express.json())
 
 app.use("/users", userRoutes)
 app.use("/admin", adminRoutes)
-app.use("/subscription", subscriptionRoutes)
+app.use("/subscriptions", subscriptionRoutes)
+
+mongoose.connect(process.env.MONGO_URI)
+.then(() => {
+    console.log("MongoDB connected");
+    app.listen(process.env.PORT, () => {
+        console.log(`SERVER RUNNING ON PORT ${process.env.PORT}` );
+    })
+}).catch(err => console.log(err))
