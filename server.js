e0e4dbb25f9b704728c13d52c8c766a1cc2eee69
zinebsektaoui <zinebsektaoui1@gmail.com>
2026-03-12 11:54:43 +0000

    Removing mongodb connexion form file

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express")
 require("dotenv").config()
-const mongoose = require("mongoose")
+const db = require("./config/db")
 
 const app = express()
 
@@ -9,15 +9,13 @@ const adminRoutes = require("./routes/admin.route")
 const subscriptionRoutes = require("./routes/subscription.route")
 
 app.use(express.json())
+db()
 
 app.use("/users", userRoutes)
 app.use("/admin", adminRoutes)
 app.use("/subscriptions", subscriptionRoutes)
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-        console.log(`SERVER RUNNING ON PORT ${process.env.PORT}` );
-    })
-}).catch(err => console.log(err))
+
+app.listen(process.env.PORT, () => {
+    console.log(`SERVER RUNNING ON PORT ${process.env.PORT}`); 
+})
