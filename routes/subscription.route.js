15138ee1e707f2f662a59c1539aa2e725daaf02f
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-03-14 22:11:04 +0100

    Subscription routes

--- routes/subscription.route.js
+++ routes/subscription.route.js
@@ -1,23 +1,22 @@
 const express = require("express")
-
-const { createSubscription } =require("../controllers/subscription.controller.js");
-
+const { createSubscription, displayAll, displayById,updateSubscription, deleteSubscription } = require("../controllers/subscription.controller.js");
 const middleware = require("../middlewares/auth.middleware.js");
 const router = express.Router();
 
 // CREATE subscription
 router.post("/",middleware.authMiddleware, createSubscription);
 
-// // GET all subscriptions (user connected)
-// router.get("/", authMiddleware, getSubscriptions);
-// 
+// GET all subscriptions (user connected)
+router.get("/", middleware.authMiddleware, displayAll);
+
 // // GET one subscription
-// router.get("/:id", authMiddleware, getSubscription);
+router.get("/:id", middleware.authMiddleware, displayById);
 
 // // UPDATE subscription
-// router.put("/:id", authMiddleware,validateSubscription, updateSubscription);
+//router.put("/:id", middleware.authMiddleware,validateSubscription, updateSubscription);
+router.put("/:id", middleware.authMiddleware, updateSubscription);
 
 // // DELETE subscription
-// router.delete("/:id", authMiddleware, deleteSubscription);
+router.delete("/:id", middleware.authMiddleware, deleteSubscription);
 
 module.exports = router;
