12bd4273ab73ced5754b20217d5456688942558e
zineb_sektaoui <zinebsektaoui1@gmail.com>
2026-03-15 14:37:22 +0100

    supprression des imports inutilisables

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,7 +3,7 @@ const router = express.Router()
 const controller = require("../controllers/user.controller")
 const subscription = require("../controllers/subscription.controller")
 // const middleware = require("../middlewares/role.middleware")
-const {checkRole} = require("../middlewares/role.middleware")
+const {roleMiddleware} = require("../middlewares/role.middleware")
 
 router.post("/signUp", controller.signUp)
 router.post("/signIn",controller.signIn)
